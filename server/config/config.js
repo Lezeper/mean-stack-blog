d5a5c097d48c55d13d531915bcf8655fa5894e63
Ma Lewis <lewis_studio@yahoo.com>
2016-06-21 08:36:04 -0400

    minor change

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,6 @@
 
 module.exports = {
-    "database": "mongodb://lewis:m!3)VexPe/zt2Gdh@ds013584.mlab.com:13584/blog",
-    "secretKey": "/xq8y-5Ve$FDDE7!"
+    "port": 8080,
+    "database": XXX,
+    "secretKey": XXX
 };
